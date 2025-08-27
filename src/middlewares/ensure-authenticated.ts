11478c643a496d9d1d5b70a629daedc3e2a36027
João Paulo Dagostin <107184368+jotadagostin@users.noreply.github.com>
2025-08-27 10:37:15 +0200

    types

---
+++ src/middlewares/ensure-authenticated.ts
@@ -0,0 +1,39 @@
+import { Request, Response, NextFunction } from "express";
+import { verify } from "jsonwebtoken";
+import { authConfig } from "@/configs/auth";
+import { AppError } from "@/utils/AppError";
+
+interface TokenPayLoad {
+  role: string;
+  sub: string;
+}
+
+function ensureAuthenticated(
+  request: Request,
+  response: Response,
+  next: NextFunction
+) {
+  try {
+    const authHeader = request.headers.authorization;
+
+    if (!authHeader) {
+      throw new AppError("Invalid JWT Token", 401);
+    }
+
+    const [, Token] = authHeader.split(" ");
+
+    const { role, sub: user_id } = verify(
+      Token,
+      authConfig.jwt.secret
+    ) as TokenPayLoad;
+
+    request.user = {
+      id: user_id,
+      role,
+    };
+
+    return next();
+  } catch (error) {
+    throw new AppError("Invalid JWT Token", 401);
+  }
+}
